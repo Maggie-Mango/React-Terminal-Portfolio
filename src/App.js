6fd70484a0229c43f716b97b7cda1f1a169b815f
Maggie Saldivia <Maggiesaldivia@gmail.com>
2022-03-15 23:38:33 -0400

    added placeholders for the portfolio examples

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const instructions =
     Resume&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     Soundtok&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     Atelier&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    API:Q&A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    APIquestions&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
   <br />
   <br />
 </>
@@ -32,27 +32,64 @@ const instructions =
 const terminal = [
   {type: LineType.Output, value: Openingmessage},
   {type: LineType.Output, value: instructions},
-  {type: LineType.Input, value: 'Get in touch 💌maggiesaldivia@gmail.com' }
+  {type: LineType.Input, value: 'Get in touch 💌 maggiesaldivia@gmail.com' }
 ]
 
 const TerminalController = (props = {}) => {
   const [terminalLineData, setTerminalLineData] = useState(terminal);
-  const [openLink, setOpenLink] = useState(null)
-  // Terminal has 100% width by default so it should usually be wrapped in a container div
+  const [modal, setModal] = useState(null)
 
+  if (modal === 'resume') {
+    terminal.push( {type: LineType.Output, value: `$ visiting Resume...`} )
+    return (
+      <>
+      <p>resume placeholder</p>
+    </>
+    )
+  }
 
-  console.log(openLink)
+  if (modal === 'soundtok') {
+    terminal.push( {type: LineType.Output, value: `$ visiting SoundTok...`} )
+    return (
+      <>
+      <p>soundtok placeholder</p>
+    </>
+    )
+  }
+
+  if (modal === 'atelier') {
+    terminal.push( {type: LineType.Output, value: `$ visiting Atelier...`} )
+    return (
+      <>
+      <p>atelier placeholder</p>
+    </>
+    )
+  }
+
+  if (modal === 'apiquestions') {
+    terminal.push( {type: LineType.Output, value: `$ visiting API questions...`} )
+    return (
+      <>
+      <p>backend placeholder</p>
+    </>
+    )
+  }
+
   return (
     <div className="container">
       <Terminal
         name='MAGGIE SALDIVIA PORTFOLIO'
         colorMode={ ColorMode.Light }
         lineData={ terminalLineData }
-        onInput={ terminalInput => { terminal.indexOf(terminalInput) > -1 ? terminal.push( {type: LineType.Output, value: terminalInput}  ) : terminal.push( {type: LineType.Output, value: `-bash:  ${terminalInput}: command not found`} )}
+        onInput={ terminalInput => {
+          terminal.indexOf(terminalInput) > -1 ? terminal.push( {type: LineType.Output, value: terminalInput}  ) : terminal.push( {type: LineType.Output, value: `-bash:  ${terminalInput}: command not found`} )
+          setModal(terminalInput)
+        }
         }
       />
     </div>
   )
+
 }
 
 export default TerminalController
