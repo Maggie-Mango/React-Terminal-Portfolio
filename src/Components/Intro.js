6a5b28cd142db7e38012f64fab6777148451dcd1
Maggie Saldivia <Maggiesaldivia@gmail.com>
2022-03-18 15:22:39 -0400

    created global Intro component

---
+++ src/Components/Intro.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+
+
+
+const Intro = ({inputCb, title, inputLine}) => {
+
+  const [input, setInput] = useState("")
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  },[])
+
+  useEffect(() => {
+    inputCb(input)
+  },[input])
+
+  return (
+    <>
+      <p id="title">{title}</p>
+      <p><span id="moneysign">$ </span><span dangerouslySetInnerHTML={{ __html: inputLine }}></span></p>
+      <p><span id="moneysign">$ </span>Typing in "back" will automatically take you to the main page <input type="text" onChange={e => setInput(e.target.value)} ref={inputRef}  value={input} /></p>
+    </>
+  )
+}
+
+export default Intro;
